3806bc4257314b41028bb3c29535ea71282f6aa3
LeKoArts <lekoarts@gmail.com>
2018-06-03 23:20:17 +0200

    Move wave

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -62,6 +62,22 @@ const Content = styled(ParallaxLayer)`
   }
 `;
 
+const WaveWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  transform: matrix(1, 0, 0, -1, 0, 0);
+`;
+
+const InnerWave = styled.div`
+  position: relative;
+  height: 100%;
+  svg {
+    width: 100%;
+    height: 30vh;
+  }
+`;
+
 const waveOne = keyframes`
   0% {
     d: path("M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z");
@@ -110,19 +126,7 @@ const Index = () => (
   <React.Fragment>
     <SEO />
     <Page>
-      <Parallax pages={3}>
-        <Divider fill="#23262b" speed={0.2} offset={0}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" style={{ height: '100vh' }}>
-            <path className={waveOneAnimation}>
-              <animate
-                attributeName="d"
-                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                repeatCount="indefinite"
-                dur="30s"
-              />
-            </path>
-          </svg>
-        </Divider>
+      <Parallax pages={4}>
         <Divider speed={0.2} offset={0}>
           <UpDown>
             <SVG icon="triangle" width={width['48']} stroke={colors.orange} left="10%" top="20%" />
@@ -162,7 +166,6 @@ const Index = () => (
           <UpDown>
             <SVG icon="box" width={width['6']} fill={colors['grey-darkest']} left="50%" top="75%" />
             <SVG icon="upDown" width={width['8']} fill={colors['grey-darkest']} left="70%" top="20%" />
-            <SVG icon="upDown" width={width['24']} fill={colors.orange} left="80%" top="80%" />
             <SVG icon="triangle" width={width['8']} stroke={colors['grey-darkest']} left="25%" top="5%" />
             <SVG icon="circle" width={width['24']} fill={colors['grey-darkest']} left="10%" top="60%" />
           </UpDown>
@@ -187,6 +190,7 @@ const Index = () => (
             <SVG icon="box" width={width['6']} fill={colors.blue} left="50%" top="75%" />
             <SVG icon="upDown" width={width['8']} fill={colors['grey-darkest']} left="70%" top="20%" />
             <SVG icon="triangle" width={width['8']} stroke={colors['grey-darkest']} left="25%" top="5%" />
+            <SVG icon="upDown" width={width['24']} fill={colors.orange} left="80%" top="80%" />
           </UpDown>
           <UpDownWide>
             <SVG icon="arrowUp" width={width['16']} fill={colors.purple} left="5%" top="80%" />
@@ -203,6 +207,40 @@ const Index = () => (
         <Content speed={0.4} offset={2}>
           <h1>Slide 3 - This is a long heading Slide 3 - This is a long heading</h1>
         </Content>
+        <Divider fill="#23262b" speed={0.2} offset={3}>
+          <WaveWrapper>
+            <InnerWave>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
+                <path className={waveOneAnimation}>
+                  <animate
+                    attributeName="d"
+                    values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
+                    repeatCount="indefinite"
+                    dur="30s"
+                  />
+                </path>
+              </svg>
+            </InnerWave>
+          </WaveWrapper>
+        </Divider>
+        <Content speed={0.4} offset={3}>
+          <h1>Slide 4 - This is a long heading Slide 4 - This is a long heading</h1>
+        </Content>
+        <Divider speed={0.1} offset={3}>
+          <UpDown>
+            <SVG icon="upDown" width={width['8']} fill={colors['grey-darkest']} left="70%" top="20%" />
+            <SVG icon="triangle" width={width['8']} stroke={colors['grey-darkest']} left="25%" top="5%" />
+          </UpDown>
+          <UpDownWide>
+            <SVG icon="triangle" width={width['12']} stroke={colors.white} left="95%" top="50%" />
+            <SVG icon="circle" width={width['6']} fill={colors.white} left="85%" top="15%" />
+            <SVG icon="upDown" width={width['8']} fill={colors['grey-darkest']} left="45%" top="10%" />
+          </UpDownWide>
+          <SVG icon="circle" width={width['6']} fill={colors.white} left="4%" top="20%" />
+          <SVG icon="circle" width={width['12']} fill={colors['grey-darkest']} left="70%" top="60%" />
+          <SVG icon="box" width={width['12']} fill={colors['grey-darkest']} left="20%" top="30%" />
+          <SVG icon="hexa" width={width['8']} stroke={colors['grey-darkest']} left="80%" top="70%" />
+        </Divider>
       </Parallax>
     </Page>
   </React.Fragment>
