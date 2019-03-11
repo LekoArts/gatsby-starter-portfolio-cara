672fa9a21cec3ba3d696d0f18f214415e211a3df
LekoArts <lekoarts@gmail.com>
2019-03-11 16:52:21 +0100

    add new svg icon: cross

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -25,6 +25,7 @@ const Hero = ({ children, offset }) => (
         <SVG icon="triangle" width={12} stroke={colors.white} left="90%" top="50%" />
         <SVG icon="circle" width={16} fill={colors['grey-darker']} left="70%" top="90%" />
         <SVG icon="triangle" width={16} stroke={colors['grey-darkest']} left="30%" top="65%" />
+        <SVG icon="cross" width={16} stroke={colors['pink-light']} left="28%" top="15%" />
         <SVG icon="circle" width={6} fill={colors['grey-darkest']} left="75%" top="10%" />
         <SVG icon="upDown" hiddenMobile width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
       </UpDownWide>
