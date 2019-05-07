4f43a8e09d403c6c91729466c0657e1498a7a613
LekoArts <lekoarts@gmail.com>
2019-05-07 12:20:27 +0200

    add config file

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    screens: {
+      xxl: '1600px',
+    },
+    fontFamily: {
+      sans: [
+        'Open Sans',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ['Cantata One', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+    },
+  },
+}
